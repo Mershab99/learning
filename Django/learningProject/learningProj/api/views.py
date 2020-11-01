d6757d7bcb8f60b51914183ef2364adb05b8e687
Mershab <mershab99@gmail.com>
2020-11-01 01:34:02 -0500

    DjangoRESTFramework + Refactored Code + Dockerized

---
+++ Django/learningProject/learningProj/api/views.py
@@ -0,0 +1,56 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from db.models import User
+
+from db.serializers import UserSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'Users':'/users/',
+        'Detail Users':'/user/<int:pk>/',
+        'Create':'/user-create/',
+        'Modify':'/user-update/<int:pk>/',
+        'Delete':'/user-delete/<int:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def userList(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def userDetail(request, pk):
+    users = User.objects.get(id=pk)
+    serializer = UserSerializer(users, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def userCreate(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def userUpdate(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def userDelete(request, pk):
+    user = User.objects.get(id=pk)
+    user.delete()
+
+    return Response('User succesfully deleted!')
