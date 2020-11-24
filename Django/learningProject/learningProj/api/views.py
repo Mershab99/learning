860ac0594248f1dd5a03dd3f3c5b54b2c5e356d0
Mershab <mershab99@gmail.com>
2020-11-23 23:26:52 -0500

    Image Upload

--- Django/learningProject/learningProj/api/views.py
+++ Django/learningProject/learningProj/api/views.py
@@ -1,23 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import FileUploadParser
 
 from db.models import User
 
 from db.serializers import UserSerializer
+
 
 # Create your views here.
 
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'Users':'/users/',
-        'Detail Users':'/user/<int:pk>/',
-        'Create':'/user-create/',
-        'Modify':'/user-update/<int:pk>/',
-        'Delete':'/user-delete/<int:pk>/',
+        'Users': '/users/',
+        'Detail Users': '/user/<int:pk>/',
+        'Create': '/user-create/',
+        'Modify': '/user-update/<int:pk>/',
+        'Delete': '/user-delete/<int:pk>/',
     }
     return Response(api_urls)
+
 
 @api_view(['GET'])
 def userList(request):
@@ -25,20 +26,22 @@ def userList(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def userDetail(request, pk):
     users = User.objects.get(id=pk)
     serializer = UserSerializer(users, many=False)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
 def userCreate(request):
-    parser_class = (FileUploadParser,)
     serializer = UserSerializer(data=request.data)
 
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 def userUpdate(request, pk):
@@ -49,6 +52,7 @@ def userUpdate(request, pk):
         serializer.save()
 
     return Response(serializer.data)
+
 
 @api_view(['DELETE'])
 def userDelete(request, pk):
