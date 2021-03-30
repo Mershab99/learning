b53e0a82621ca8e01de3702d56ee372910f23db4
Mershab <mershab99@gmail.com>
2021-03-29 23:29:23 -0400

    YT_Downloader

--- Python/Selenium/main.py
+++ Python/Selenium/main.py
@@ -1,15 +1,41 @@
-from selenium import webdriver
+import os
+import sys
 from time import sleep
 
+import requests
+from selenium import webdriver
 
-class Scraper:
+
+class YouTubeDownloader:
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path='G:/selenium/chromedriver.exe')
-        self.driver.get("http://google.com")
-        sleep(2)
-        #quote = self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[7]/div[2]/div/p/span[1]')
-        #author = self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[7]/div[2]/div/p/span[2]/span[1]')
-        x = 1
+        self.driver = webdriver.Chrome(executable_path=os.environ.get('WEBDRIVER'))
+
+        for x in sys.argv:
+            if x.__contains__(".py"):
+                continue
+            self.driver.get(os.environ.get('YT_DOWNLOADER_URL'))
+            self.download(x)
+
+    def download(self, link):
+        # FILL WITH VALUE
+        self.driver.find_element_by_xpath('/html/body/div/div[1]/form/input').send_keys(link)
+        # CLICK THE CONVERT BUTTON
+        self.driver.find_element_by_xpath('/html/body/div/div[1]/form/button').click()
+        sleep(5)
+        title = self.driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div/div/div/div/h3').text
+        creator = self.driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div/div/div/div/p[1]').text
+        # CLICK THE 'Find Link' button
+        self.driver.find_element_by_xpath('/html/body/div/div[1]/div[2]/div/div/div/div/div/div[1]/button').click()
+        sleep(5)
+        # GET DOWNLOAD URL
+        download_url = self.driver.find_element_by_xpath(
+            '/html/body/div/div[1]/div[2]/div/div/div/div/div/div[1]/a[1]').get_attribute('href')
+        # make download request
+        req = requests.get(download_url, allow_redirects=True)
+        # construct download path
+        path = os.environ.get('DOWNLOAD_FOLDER', default='') + title + ' - ' + creator + '.mp4'
+        file = open(path, "wb")
+        file.write(req.content)
 
 
-Scraper()
+YouTubeDownloader()
